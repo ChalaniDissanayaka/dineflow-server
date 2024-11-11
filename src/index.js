7f88fee546bfb0d44a1afbb26515719b1c61cd7f
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-11-11 22:00:42 +1100

    fix: CORS error in index file

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://dineflow-restaurant-app.netlify.app"],
   })
 );
 app.use(
